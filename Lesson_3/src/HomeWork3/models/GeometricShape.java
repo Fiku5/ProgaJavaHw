d442e262d810cdc254b7fcff53ac5597a74a03da
pavel <pavelnikolaev2101@gmail.com>
2025-02-10 13:02:16 +0300

    delete all files

--- Lesson_3/src/HomeWork3/models/GeometricShape.java
+++
@@ -1,6 +0,0 @@
-package HomeWork3.models;
-
-public abstract class GeometricShape {
-    public abstract double getPerimeter();
-    public abstract double getArea();
-}
--- hw_4-5/src/ru/itis/users/notifiers/AbstractNotifier.java
+++
@@ -1,8 +0,0 @@
-package ru.itis.users.notifiers;
-
-import ru.itis.users.models.User;
-
-public abstract class AbstractNotifier {
-
-    public abstract void notifyUser(User user, String message);
-}
